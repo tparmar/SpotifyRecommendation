e7a07ec849729ef57d04a408ae26de996ee0cd67
Teerthraj Parmar <helloparmars@gmail.com>
2024-06-03 12:39:56 -0400

    Added forms for song component

--- frontend_spotify/src/components/Song.tsx
+++ frontend_spotify/src/components/Song.tsx
@@ -3,8 +3,35 @@ import { Fragment } from "react";
 
 function Song() {
   return (
-    <div>
-      <h1>Song recommendation in progress</h1>
+    <div className="position-absolute top-50 start-50 translate-middle">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="songName" className="form-label">
+            Song
+          </label>
+          <input
+            type="search"
+            className="form-control"
+            id="songName"
+            aria-describedby="songSearch"
+            placeholder="Search for song..."
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="numRecommendations" className="form-label">
+            Recommendations
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="numRecommendations"
+            placeholder="# of Suggestions"
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
